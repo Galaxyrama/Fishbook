4cc77616aa5a972ee0c89c20f9b9aa6804be723b
Joshua Aldueza <alduezajoshua@gmail.com>
2025-04-07 10:13:05 +0800

    Designed the Not Found page

--- frontend/src/pages/PageNotFound.jsx
+++ frontend/src/pages/PageNotFound.jsx
@@ -4,15 +4,21 @@ import { Link } from 'react-router-dom'
 
 const PageNotFound = () => {
   return (
-    <div>
+    <div className='bg-background font-montagu min-h-screen'>
       <Navbar />
-      <h1>Page not found</h1>
-      <p>Sorry, the page you are looking for cannot be found.
-        Please check the URL or try navigating back to the homepage.
-      </p>
-      <Link to={"/"}> 
-        <p>Go back to homepage &rarr;</p>
-      </Link>
+      <div className='flex items-center justify-center w-full'>
+        <div className='text-center max-w-100 mt-25'>
+          <h1 className='font-bold text-4xl mb-5'>404</h1>
+          <img src='/images/PageNotFoundImg.png'/>
+          <h1 className='font-semibold text-2xl mb-5 mt-5'>Page not found</h1>
+          <p className='max-w-90 mx-auto'>Sorry, the page you are looking for cannot be found.
+            Please check the URL or try navigating back to the homepage.
+          </p>
+          <Link to={"/"}> 
+            <p className=' bg-btn text-white py-3 rounded-lg mt-7'>Go back to homepage &rarr;</p>
+          </Link>
+        </div>
+      </div> 
     </div>
   )
 }
