9e490bf954e4250b1b91d078917f272b65ce10be
Joshua Aldueza <alduezajoshua@gmail.com>
2025-04-09 11:17:28 +0800

    Added tooltip for Post on the engagement

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,18 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
+import { Tooltip } from 'flowbite';
 
 const Post = ( { User, Img, DateUpload, ProfilePic } ) => {
   const date = new Date(DateUpload);
+  const tooltipCommentId = `tooltip-comment-${User}-${DateUpload}`;
+  const tooltipLikeId = `tooltip-like-${User}-${DateUpload}`;
+
+  const [isOpen, setIsOpen] = useState(false);
 
   const formattedDate = date.toLocaleDateString("en-US", {
     month: "2-digit",
     day: "2-digit",
     year: "numeric",
   });
+
+  const handleShareLink = () => {
+    navigator.clipboard.writeText(`${window.location.href}${User}/status/1`);
+    setIsOpen(true);
+
+    setTimeout(() => setIsOpen(false), 5000);
+  }
+
+  useEffect(() => {
+    const $targetE1 = document.getElementById(tooltipCommentId);
+    const $triggerE1 = document.querySelector(`[data-tooltip-target="${tooltipCommentId}"]`);
+
+    const $targetE2 = document.getElementById(tooltipLikeId);
+    const $triggerE2 = document.querySelector(`[data-tooltip-target="${tooltipLikeId}"]`);
+
+    if($targetE1 && $triggerE1) {
+      new Tooltip($targetE1, $triggerE1);
+    }
+
+    if($targetE2 && $triggerE2) {
+      new Tooltip($targetE2, $triggerE2);
+    }
+  }, [])
   
   return (
-    <div className='font-montagu'>
-        <div className='flex justify-center w-full pb-5'>
+    <div className='font-montagu px-2 h-full'>
+      <div className='flex justify-center w-full pb-5'>
         <div className='w-full max-w-3xl pb-2'>
           <div className='flex gap-4 rounded-lg drop-shadow-xl py-6 bg-white px-4 '>
             <div className='w-12 flex-shrink-0'>
@@ -28,30 +56,75 @@ const Post = ( { User, Img, DateUpload, ProfilePic } ) => {
               
               <div>
                 {/* Text Post */}
-                <p className='text-justify pb-3 cursor-pointer'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, eaque maxime. Ex, 
-                  earum vitae, blanditiis perferendis voluptate eaque dolore porro magni facilis 
-                  consequuntur totam omnis odio perspiciatis recusandae mollitia quo?</p>
+                <Link to={`/${User}/status/1`}>
+                  <p className='text-justify pb-3 cursor-pointer'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, eaque maxime. Ex, 
+                    earum vitae, blanditiis perferendis voluptate eaque dolore porro magni facilis 
+                    consequuntur totam omnis odio perspiciatis recusandae mollitia quo?</p>
+                </Link>
                   {/* Image Post */}
                 {Img && <img src={Img} alt="" className='pb-3 w-full'/>}
 
                 <hr className='pt-3'/>
 
-                <div className='flex justify-between sm:px-20'>
-                  <div className='flex select-none cursor-pointer'>
+                <div className='flex justify-evenly'>
                   {/* Comments */}
+                  <div 
+                    className='flex select-none cursor-pointer'
+                    data-tooltip-target={tooltipCommentId}>
                     <img src="/images/comment.png" alt="" className='w-6 h-6 mr-1'/>
-                    <p># of comments</p> 
+                    <p>590</p> 
                   </div>
-                  <div className='flex select-none cursor-pointer'>
-                    {/* Likes */}
+                  {/* Likes */}
+                  <div 
+                    className='flex select-none cursor-pointer'
+                    data-tooltip-target={tooltipLikeId}>
                     <img src="/images/heart.png" alt="" className='w-6 h-6 mr-1'/>
-                    <p># of likes</p>
+                    <p>8770</p>
+                  </div>
+                  {/* Share Link */}
+                  <div 
+                    className='flex select-none cursor-pointer'
+                    onClick={handleShareLink}>
+                    <img src="/images/share.png" className='w-g h-6 mr-1' />
+                    <p>Share</p>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
+      </div>
+      {/* Modal for sharing link */}
+      {isOpen && 
+        <div className='z-50 bottom-4 left-1/2 transform -translate-x-1/2 text-center w-60 fixed'>
+          <p className='text-white bg-btn py-2 rounded-md'>Copied to clickboard</p>
+        </div>
+      }
+
+      {/* Tooltip for Comment */}
+      <div
+        id={tooltipCommentId}
+        role="tooltip"
+        className="absolute z-10 invisible 
+                   inline-block px-3 py-2 text-sm 
+                   font-medium bg-btn 
+                   text-white rounded-lg shadow-sm 
+                   opacity-0 transition-opacity 
+                   duration-300 tooltip">
+          Comment
+      </div>
+      
+      {/* Tooltip for Like */}
+      <div
+        id={tooltipLikeId}
+        role="tooltip"
+        className="absolute z-10 invisible
+                   inline-block px-3 py-2 text-sm 
+                   font-medium bg-btn 
+                   text-white rounded-lg shadow-sm 
+                   opacity-0 transition-opacity 
+                   duration-300 tooltip">
+          Like
       </div>
     </div>
   )
